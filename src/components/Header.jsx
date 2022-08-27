bcd090c4747ed6eea94be6c409e2a46dbcf92231
john-glay <johnglayc.bunao@gmail.com>
2022-08-27 20:39:20 +0800

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+
+// Icons
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpIcon from "@mui/icons-material/Help";
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <HeaderAvatar
+          src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+          alt="pickachu image"
+        />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Seach in +Ultra's Channel" />
+      </HeaderSearch>
+
+      <HeaderRight>
+        <HelpIcon />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin: 5px 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #412f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIpEQUrb22_8YCf8hnj2_woCXhbRhdcqQ",
+  authDomain: "react-slack-59273.firebaseapp.com",
+  projectId: "react-slack-59273",
+  storageBucket: "react-slack-59273.appspot.com",
+  messagingSenderId: "1096392586098",
+  appId: "1:1096392586098:web:38c2385867b56b019d23eb"
+};
+
+  // Use this to initialize the firebase App
+  const firebaseapp = firebase.initializeApp(firebaseConfig);
+
+  // Use these for db, auth & provider
+  const db = firebaseapp.firestore();
+
+  export { db }
